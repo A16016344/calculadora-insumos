450f4a8e42bb5dd5eec02b67b575168d5f6ff849
A16016344 <47512215+A16016344@users.noreply.github.com>
2019-08-10 00:55:18 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var cacheName = 'Offline_0719';
+var cacheName = 'Offline_0819';
 var filesToCache = [
 '.',
 	'css/css.css',
